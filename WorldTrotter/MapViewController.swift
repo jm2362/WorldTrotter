e4f37686789790d0bfa4bb392a452bf0b2559ed2
Jeff Meehan <Home@jacks-macbook-pro.local>
2016-08-28 13:34:23 -0500

    Creating a view Programmatically

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+	// When a view controller is created, its view property is nil.
+	var mapView: MKMapView!
+	
+	// If a view controller is asked for its view and its vies is nil,
+	// then the loadView method is called.
+	override func loadView() {
+		// Create a map view.
+		mapView = MKMapView()
+		
+		// Set it as *the* view of this view controller.
+		view = mapView
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
 		print("MapViewController loaded its view")
 	}
+	
 }
